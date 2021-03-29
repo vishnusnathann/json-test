fc3691ca8683b78e3f67516cc273a1d0f8b2bc9f
vishnath <vishnusnathan@gmail.com>
2021-03-29 13:29:10 +0530

    Completed question2 , partial result for question1

--- question1/index.js
+++ question1/index.js
@@ -136,21 +136,58 @@ const input1 = [
         
 
 
+// declaring ouput object
 let output = [];
+
+
+const childrenFinder = (child_object,key,input1) =>{
+    temp_output = [];
+
+    const temp_resultPromise = new Promise(async (res,rej)=>{
+        await child_object.map(async (child_object_item)=>{
+            let temp_item = {};
+            temp_item.title = child_object_item.title
+            if(child_object_item.children == undefined){
+                await input1.map(async (input1_item,index)=>{
+                    temp_item =  {...temp_item,[`data${index}`]: input1_item.node[child_object_item.key]}
+                    
+                })
+            }
+            else{
+                
+                temp_item = {...temp_item, children: childrenFinder(child_object_item.children,child_object_item.key,input1_item.node[input2_item.key].edges)}
+            }
+    
+            temp_output.push(temp_item);
+        });
+            res("done")
+    });
+
+    temp_resultPromise.then(res =>{
+        return(temp_output)
+    })
+
+}
+
 
 const resultPromise = new Promise(async (res,rej)=>{
     await input2.map(async (input2_item)=>{
         let temp_item = {};
         temp_item.title = input2_item.title
-        // console.log(temp_item);
-        await input1.map(async (input1_item,index)=>{
-            temp_item =  {...temp_item,[`data${index}`]: input1_item.node[input2_item.key]}
-            
-        })
+        if(input2_item.children == undefined){
+            await input1.map(async (input1_item,index)=>{
+                temp_item =  {...temp_item,[`data${index}`]: input1_item.node[input2_item.key]}
+                
+            })
+        }
+        else{
+            await input1.map(async (input1_item,index)=>{
+                if(input1_item.node[input2_item.key])
+                    temp_item = {...temp_item, children: childrenFinder(input2_item.children,input2_item.key,input1_item.node[input2_item.key].edges)}
+            })
+        }
 
-        // await console.log(temp_item);
         output.push(temp_item);
-        // await console.log(output);
     });
         res("done")
 })
